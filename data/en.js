3215440a839335e7b6c27d1d5809038e05d5d55e
SiSOustE <ryzhevsky@gmail.com>
2024-12-23 04:14:08 +0300

    modules / error: an import declaration can only be used at the top level of a module

---
+++ data/en.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+      title: "Registration",
+      usernameLabel: "Username:",
+      passwordLabel: "Password:",
+      submitButton: "Register",
+      toggleButton: "RU",
+      successMessage: "Registration successful!",
+      errorMessage: "Please agree to the terms."
+  }
+};
---
+++ data/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+      title: "Регистрация",
+      usernameLabel: "Имя пользователя:",
+      passwordLabel: "Пароль:",
+      submitButton: "Зарегистрироваться",
+      toggleButton: "EN",
+      successMessage: "Регистрация успешна!",
+      errorMessage: "Пожалуйста, согласитесь с условиями."
+  }
+};
